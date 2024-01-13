1c215662853167a75885d0304ee907718cadede3
Krystian <63970452+NeBq2109@users.noreply.github.com>
2024-01-13 18:53:58 +0100

    Time: 0 ms (100.00%), Space: 6.7 MB (76.10%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    stack<char> stack;
+
+    for (const char& c : s)
+    {
+        if (c == '(') stack.push(')');
+        else if (c == '{') stack.push('}');
+        else if (c == '[') stack.push(']');
+        else if (stack.empty() || stack.top()!=c) return false;
+        else stack.pop();
+    }
+    return stack.empty();
+    }
+};
